5afb5e5576ff41000ba5eefb9b0e95313cb1e474
Humaipek <153774907+Humaipek@users.noreply.github.com>
2024-04-26 22:27:03 +0200

    4.26

--- src/test/java/test_case01/TC03.java
+++ src/test/java/test_case01/TC03.java
@@ -1,10 +1,131 @@
 package test_case01;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import java.time.Duration;
 
 public class TC03 {
     @Test
-    void tc03() {
+    void tc03_1() throws InterruptedException {
+        WebDriver driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+        driver.get("https://automationexercise.com");
+        Thread.sleep(3000);
+
+        Assertions.assertEquals(driver.getCurrentUrl(),"https://automationexercise.com/");
+
+        WebElement link= driver.findElement(By.xpath("//a[@href='/login']/i"));
+        link.click();
+
+        WebElement name=driver.findElement(By.xpath("//input[@data-qa='signup-name'] "));
+        name.sendKeys("yuniel");
+
+        WebElement email=driver.findElement(By.xpath("//input[@data-qa='signup-email']"));
+        email.sendKeys("ivory.mathayus@foodfarms.net");
+
+        WebElement enter=driver.findElement(By.xpath("//button[@data-qa='signup-button']"));
+        enter.click();
+
+        WebElement titleMrs= driver.findElement(By.id("id_gender2"));
+        titleMrs.click();
+
+        WebElement userName=driver.findElement(By.id("name"));
+        userName.clear();
+        userName.sendKeys("yuniel");
+
+        WebElement password=driver.findElement(By.id("password"));
+        password.sendKeys("Dunya1234");
+
+        WebElement ddm= driver.findElement(By.id("days"));
+        Select optionsDay=new Select(ddm);
+        optionsDay.selectByVisibleText("24");
+
+        WebElement ddm2= driver.findElement(By.id("months"));
+        Select optionsMonth=new Select(ddm2);
+        optionsMonth.selectByVisibleText("September");
+
+        WebElement ddm3= driver.findElement(By.id("years"));
+        Select optionsYear=new Select(ddm3);
+        optionsYear.selectByVisibleText("1978");
+
+
+        WebElement checkBox=driver.findElement(By.id("newsletter"));
+        checkBox.click();
+
+        WebElement receive=driver.findElement(By.id("optin"));
+        receive.click();
+
+        WebElement firstName=driver.findElement(By.id("first_name"));
+        firstName.sendKeys("yuniel");
+
+        WebElement lastName=driver.findElement(By.id("last_name"));
+        lastName.sendKeys("kodey");
+
+        WebElement companyName=driver.findElement(By.id("company"));
+        companyName.sendKeys("kodey");
+
+        WebElement address1=driver.findElement(By.id("address1"));
+        address1.sendKeys("kodey");
+
+        WebElement address2=driver.findElement(By.id("address2"));
+        address2.sendKeys("kodey");
+
+        WebElement ddm4= driver.findElement(By.id("country"));
+        Select optionsCountry=new Select(ddm4);
+        optionsCountry.selectByVisibleText("India");
+
+        WebElement state=driver.findElement(By.id("state"));
+        state.sendKeys("kodey");
+
+        WebElement city=driver.findElement(By.id("city"));
+        city.sendKeys("kodey");
+
+        WebElement zipcode=driver.findElement(By.id("zipcode"));
+        zipcode.sendKeys("3244");
+
+        WebElement mobil=driver.findElement(By.id("mobile_number"));
+        mobil.sendKeys("3244226677");
+
+        WebElement create=driver.findElement(By.xpath("//button[@data-qa='create-account']"));
+        create.click();
+
+        driver.quit();
+
+    }
+
+    @Test
+    void tc03_2() throws InterruptedException {
+        WebDriver driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+        driver.get("https://automationexercise.com");
+        Thread.sleep(3000);
+
+        Assertions.assertEquals(driver.getCurrentUrl(),"https://automationexercise.com/");
+
+        WebElement link= driver.findElement(By.xpath("//a[@href='/login']/i"));
+        link.click();
+
+        WebElement loginAcccount= driver.findElement(By.xpath("//h2[.='Login to your account']"));
+        Assertions.assertTrue(loginAcccount.isDisplayed());
+
+        WebElement email=driver.findElement(By.cssSelector("input[data-qa='login-email']"));
+        email.sendKeys("kerby.kaidyn@foodfarms.net");
+
+        WebElement password=driver.findElement(By.cssSelector("input[data-qa='login-password']"));
+        password.sendKeys("Dunya1234");
+
+        WebElement enter=driver.findElement(By.cssSelector("button[data-qa='login-button'] "));
+        enter.click();
 
     }
 }
